341506f92fb37b8d3a365eedc265cbe7701aa74f
Markus Kollers <m.kollers@vertical.de>
2019-01-16 23:40:08 +0100

    Fix app component test

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,16 +1,27 @@
-import { TestBed, async } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { async, TestBed } from '@angular/core/testing';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
+
+import { DomSanitizerMock } from '../mocks/dom-sanitizer.mock';
+import { MatIconRegistryMock } from '../mocks/mat-icon-registry.mock';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule.withRoutes([])
       ],
       declarations: [
         AppComponent
       ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        { provide: DomSanitizer, useClass: DomSanitizerMock },
+        { provide: MatIconRegistry, useClass: MatIconRegistryMock }
+      ]
     }).compileComponents();
   }));
 
