7d189e8424df4ba69b99d10247ce2b862a779908
Markus Kollers <m.kollers@vertical.de>
2019-01-16 21:57:04 +0100

    Fix active tabs

--- frontend/src/app/shared/layout/components/trackmania-tabs/trackmania-tabs.component.ts
+++ frontend/src/app/shared/layout/components/trackmania-tabs/trackmania-tabs.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import { Router, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-trackmania-tabs',
@@ -6,7 +7,22 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./trackmania-tabs.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TrackmaniaTabsComponent {
+export class TrackmaniaTabsComponent implements AfterViewInit {
+  isViewInitialized = false; // Remove this after the following issue is fixed: https://github.com/angular/material2/issues/11811
 
-  constructor() { }
+  constructor(
+    private _changeDetectorRef: ChangeDetectorRef,
+    private _router: Router
+  ) { }
+
+  ngAfterViewInit() {
+    this.isViewInitialized = true;
+    this._changeDetectorRef.detectChanges();
+  }
+
+  isLinkActive(rla: RouterLinkActive) {
+    const routerLink = rla.linksWithHrefs.first;
+
+    return this._router.isActive(routerLink.urlTree, false);
+  }
 }
