ba967e279fb8df844d612da3a88e75a7920a9831
Markus Kollers <mail@markuskollers.de>
2020-02-02 01:15:21 +0100

    Complete code coverage for signup

--- frontend/src/app/pages/auth/register-page/register-page.component.spec.ts
+++ frontend/src/app/pages/auth/register-page/register-page.component.spec.ts
@@ -3,13 +3,14 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { RouterTestingModule } from '@angular/router/testing';
-import { MatSnackbarMock } from 'src/mocks/mat-snackbar.mock';
+import { AuthServiceMock } from '@mocks/auth-service.mock';
+import { LoaderServiceMock } from '@mocks/loader-service.mock';
+import { AuthService } from '@shared/auth/services/auth.service';
+import { LoaderService } from '@shared/layout/services/loader.service';
+import { MatSnackbarMock } from '@mocks/mat-snackbar.mock';
 
-import { AuthServiceMock } from '../../../../mocks/auth-service.mock';
-import { LoaderServiceMock } from '../../../../mocks/loader-service.mock';
-import { AuthService } from '../../../shared/auth/services/auth.service';
-import { LoaderService } from '../../../shared/layout/services/loader.service';
 import { RegisterPageComponent } from './register-page.component';
+import { AuthErrorMessages } from '@shared/auth/data/auth-error.messages';
 
 describe('RegisterPageComponent', () => {
   let component: RegisterPageComponent;
@@ -37,5 +38,70 @@ describe('RegisterPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('submit', () => {
+    let authService: AuthService;
+
+    beforeEach(() => {
+      authService = TestBed.get(AuthService);
+    });
+
+    describe('loading animation', () => {
+      let loader: LoaderService;
+
+      beforeEach(() => {
+        loader = TestBed.get(LoaderService);
+      });
+
+      it('show loading on submit called', async () => {
+        // Arrange
+        spyOn(authService, 'register').and.stub();
+
+        // Act
+        await fixture.ngZone.run(async () => {
+          await component.submit();
+        });
+
+        // Assert
+        expect(loader.isLoading).toEqual(true);
+      });
+
+      it('hide loading on login error', async () => {
+        // Arrange
+        spyOn(authService, 'register').and.returnValue(Promise.reject('Nuclear meltdown'));
+        spyOn(console, 'error').and.stub();
+
+        // Act
+        await component.submit();
+
+        // Assert
+        expect(loader.isLoading).toEqual(false);
+      });
+    });
+
+    describe('error handling', () => {
+      let snackBar: MatSnackBar;
+
+      beforeEach(() => {
+        snackBar = TestBed.get(MatSnackBar);
+      });
+
+      // tslint:disable-next-line: forin
+      for (const code in AuthErrorMessages) {
+        it(code, async () => {
+          // Arrange
+          spyOn(authService, 'register').and.returnValue(Promise.reject({ code: code }));
+          spyOn(console, 'error').and.stub();
+          spyOn(snackBar, 'open').and.stub();
+
+          // Act
+          await component.submit();
+
+          // Assert
+          expect(snackBar.open).toHaveBeenCalledWith(AuthErrorMessages[code], '', { duration: 10000 });
+        });
+      }
+    });
   });
 });
