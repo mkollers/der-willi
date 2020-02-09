1d5b3170c68d3a39673ce2764c3e8a8ca58f6737
Markus Kollers <mail@markuskollers.de>
2020-02-09 01:48:27 +0100

    Remove flex layout import

--- frontend/src/app/pages/auth/register-page/register-page.module.ts
+++ frontend/src/app/pages/auth/register-page/register-page.module.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { AuthModule } from '@shared/auth/auth.module';
 
-import { AuthModule } from '../../../shared/auth/auth.module';
 import { RegisterPageRoutingModule } from './register-page-routing.module';
 import { RegisterPageComponent } from './register-page.component';
 
